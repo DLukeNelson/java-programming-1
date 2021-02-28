af5973fad19b0d936ded37cc83ffd49c9909ca72
Luke Nelson <david.luke.nelson@gmail.com>
2021-02-28 14:49:11 -0700

    SecondsInADay Exercise

--- part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -2,11 +2,14 @@
 import java.util.Scanner;
 
 public class SecondsInADay {
+    public static final int SECONDS_PER_DAY = 86400;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("How many days would you like to convert to seconds?");
+        int numDays = Integer.valueOf(scanner.nextLine());
+        System.out.println(numDays * SECONDS_PER_DAY);
 
     }
 }
