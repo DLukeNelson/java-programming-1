ebd294e9211ee84c2b76fdb3b97be1cad79c221b
Luke Nelson <david.luke.nelson@gmail.com>
2021-03-10 12:17:56 -0700

    CountingToHundred exercise

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -6,5 +6,10 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int counter = Integer.valueOf(scanner.nextLine());
+        while (counter <= 100) {
+            System.out.println(counter);
+            counter += 1;
+        }
     }
 }
