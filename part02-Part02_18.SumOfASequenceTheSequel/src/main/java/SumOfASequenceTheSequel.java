e3be87bdcccbdc1376abfe800136c7d259236362
Luke Nelson <david.luke.nelson@gmail.com>
2021-03-10 13:29:49 -0700

    SumOfASequenceTheSequel exercise

--- part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -6,5 +6,16 @@ public class SumOfASequenceTheSequel {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("First number? ");
+        int counter = Integer.valueOf(scanner.nextLine());
+        System.out.print("Last number? ");
+        int last = Integer.valueOf(scanner.nextLine());
+        
+        int sum = 0;
+        while (counter <= last) {
+            sum += counter;
+            counter += 1;
+        }
+        System.out.println("The sum is " + sum);
     }
 }
