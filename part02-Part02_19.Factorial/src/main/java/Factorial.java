8d3400d90bde5ef35e2236f3ac971357244b25b8
Luke Nelson <david.luke.nelson@gmail.com>
2021-03-10 13:31:27 -0700

    Factorial exercise

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,15 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Give a number: ");
+        int last = Integer.valueOf(scanner.nextLine());
+        
+        int product = 1;
+        int counter = 1;
+        while (counter <= last) {
+            product *= counter;
+            counter += 1;
+        }
+        System.out.println("Factorial: " + product);
     }
 }
