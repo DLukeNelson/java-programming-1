1090b1f504b40d4d96b3fa44f2dce67f0b6faa15
Luke Nelson <david.luke.nelson@gmail.com>
2021-03-10 12:19:41 -0700

    FromWhereToWhere exercise part 1

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -6,6 +6,12 @@ public class FromWhereToWhere {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.print("Where to? ");
+        int goal = Integer.valueOf(scanner.nextLine());
+        int counter = 1;
+        while (counter <= goal) {
+            System.out.println(counter);
+            counter += 1;
+        }
     }
 }
