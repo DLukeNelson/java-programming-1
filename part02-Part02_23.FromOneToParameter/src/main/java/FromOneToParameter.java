69203999a7c55943ab505a2fbae1e7d4b196ff57
Luke Nelson <david.luke.nelson@gmail.com>
2021-03-15 11:47:20 -0600

    FromOneToParameter exercise

--- part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -5,5 +5,13 @@ public class FromOneToParameter {
     public static void main(String[] args) {
 
     }
+    
+    public static void printUntilNumber(int number) {
+        int i = 0;
+        while (i < number) {
+            i += 1;
+            System.out.println(i);
+        }
+    }
 
 }
