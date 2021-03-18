b698659c141d28490b505b034855e98d53070d78
Luke Nelson <david.luke.nelson@gmail.com>
2021-03-18 01:51:15 -0600

    Smallest exercise

--- part02-Part02_30.Smallest/src/main/java/Smallest.java
+++ part02-Part02_30.Smallest/src/main/java/Smallest.java
@@ -5,7 +5,11 @@ public class Smallest {
         // Write some code here
         // don't print anything inside this method
         // there must be a return in the end of the method
-        return -1;
+        int smallest = number1;
+        if (number2 < number1) {
+            smallest = number2;
+        }
+        return smallest;
     }
 
     public static void main(String[] args) {
