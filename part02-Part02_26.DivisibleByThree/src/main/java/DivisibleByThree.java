1bb2740641119318b796857840b848575174b7ad
Luke Nelson <david.luke.nelson@gmail.com>
2021-03-17 21:58:25 -0600

    DivisibleByThree exercise

--- part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -7,5 +7,14 @@ public class DivisibleByThree {
         Scanner scanner = new Scanner(System.in);
 
     }
+    
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        while (beginning <= end) {
+            if (beginning % 3 == 0) {
+                System.out.println(beginning);
+            }
+            beginning += 1;
+        }
+    }
 
 }
