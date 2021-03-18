d7ac84520a4c6b9347df8c040cfed4abee9597ac
Luke Nelson <david.luke.nelson@gmail.com>
2021-03-18 11:03:50 -0600

    StarSign printRectangle method

--- part02-Part02_33.StarSign/src/main/java/StarSign.java
+++ part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -35,7 +35,10 @@ public class StarSign {
     }
 
     public static void printRectangle(int width, int height) {
-        // third part of the exercise
+        while (height > 0) {
+            printStars(width);
+            height -= 1;
+        }
     }
 
     public static void printTriangle(int size) {
