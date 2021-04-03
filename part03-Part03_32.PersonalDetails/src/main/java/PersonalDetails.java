1077e93cb44f4a239a90f6d3df11658d059e1f98
Luke Nelson <david.luke.nelson@gmail.com>
2021-04-03 08:34:51 -0600

    PersonalDetails exercise

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,27 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        int count = 0;
+        int sumOfBirthYears = 0;
+        String longestName = "";
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            String[] parts = input.split(",");
+            String name = parts[0];
+            if (name.length() > longestName.length()) {
+                longestName = name;
+            }
+            
+            int birthYear = Integer.valueOf(parts[1]);
+            sumOfBirthYears += birthYear;
+            count += 1;
+        }
+        System.out.println("Longest name: " + longestName);
+        double average = sumOfBirthYears * 1.0 / count;
+        System.out.println("Average of the birth years: " + average);
     }
 }
