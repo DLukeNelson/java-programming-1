a2b335b6f08dbf4fa61a1e1aa3921893b2457a11
Luke Nelson <david.luke.nelson@gmail.com>
2021-04-02 20:22:38 -0600

    LineByLine exercise

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,6 +5,19 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            for (String word: input.split(" ")) {
+                System.out.println(word);
+            }
+        }
+        
 
 
     }
