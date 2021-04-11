2d40ac4e8e407f782967b20632b147c141c923dd
Luke Nelson <david.luke.nelson@gmail.com>
2021-04-11 11:21:06 -0600

    Create the PaymentCard class

---
+++ part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Luke <david.luke.nelson@gmail.com>
+ */
+public class PaymentCard {
+    private double balance;
+    
+    public PaymentCard(double balance) {
+        this.balance = balance;
+    }
+    
+    public String toString() {
+        return "The card has a balance of " + this.balance + " euros";
+    }
+
+}
