03fcf6482fde90cf499dad527cd88bbb54799c90
Luke Nelson <david.luke.nelson@gmail.com>
2021-04-11 11:18:00 -0600

    Statistics exercise main tracks evens and odds

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -6,6 +6,8 @@ public class MainProgram {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Statistics allNumbers = new Statistics();
+        Statistics evenNumbers = new Statistics();
+        Statistics oddNumbers = new Statistics();
         
         System.out.println("Enter numbers: ");
         while(true) {
@@ -15,7 +17,15 @@ public class MainProgram {
             }
             
             allNumbers.addNumber(number);
+            
+            if (number % 2 == 0) {
+                evenNumbers.addNumber(number);
+            } else {
+                oddNumbers.addNumber(number);
+            }
         }
         System.out.println("Sum: " + allNumbers.sum());
+        System.out.println("Sum of even numbers: " + evenNumbers.sum());
+        System.out.println("Sum of odd numbers: " + oddNumbers.sum());
     }
 }
