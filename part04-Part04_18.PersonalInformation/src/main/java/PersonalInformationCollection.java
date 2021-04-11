8ac192c2813ca96ecf952322f11c4c035a151554
Luke Nelson <david.luke.nelson@gmail.com>
2021-04-11 15:19:03 -0600

    PersonalInformation exercise

--- part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -9,6 +9,26 @@ public class PersonalInformationCollection {
 
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.print("First name: ");
+            String firstName = scanner.nextLine();
+            if (firstName.isEmpty()) {
+                System.out.println("");
+                break;
+            }
+            
+            System.out.print("Last name: ");
+            String lastName = scanner.nextLine();
+            System.out.print("Identification number: ");
+            String idNumber = scanner.nextLine();
+            
+            infoCollection.add(new PersonalInformation(firstName, lastName, idNumber));
+        }
+        
+        for (PersonalInformation info: infoCollection) {
+            System.out.println(info.getFirstName() + " " + info.getLastName());
+        }
 
     }
 }
