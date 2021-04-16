a0aa9fbbe28ea4320a8b9829207865145d7bf6ef
Luke Nelson <david.luke.nelson@gmail.com>
2021-04-16 10:52:03 -0600

    Books exercise use Book class in main project

--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -4,9 +4,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        ArrayList<Book> books = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
         
-        // implement here the program that allows the user to enter 
-        // book information and to examine them
+        while (true) {
+            System.out.print("Title: ");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                System.out.println("");
+                break;
+            }
+            System.out.print("Pages: ");
+            int pages = Integer.valueOf(scanner.nextLine());
+            
+            System.out.print("Publication year: ");
+            int year = Integer.valueOf(scanner.nextLine());
+            
+            books.add(new Book(name, pages, year));
+        }
+        
+        System.out.print("What information will be printed? ");
+        String requestedInfo = scanner.nextLine();
+        
+        for (Book book: books) {
+            if (requestedInfo.equals("everything")) {
+                System.out.println(book);
+            } else if (requestedInfo.equals("name")) {
+                System.out.println(book.getName());
+            }
+        }
 
     }
 }
