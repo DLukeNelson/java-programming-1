b301f6acc7d6ce4567b7c0887cf14cdb297b0751
Luke <dlnelson@ualberta.ca>
2021-04-21 09:00:15 -0600

    Complete RecordsFromAFile exercise

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,19 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Name of the file?");
+        String fileName = scanner.nextLine();
+        
+        try (Scanner fileScanner = new Scanner(Paths.get(fileName))) {
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                String[] parts = line.split(",");
+                System.out.println(new Person(parts[0], Integer.valueOf(parts[1])));
+            }
+        } catch (Exception e) {
+            System.out.println("Unable to read file: " + fileName);
+        }
 
     }
 }
