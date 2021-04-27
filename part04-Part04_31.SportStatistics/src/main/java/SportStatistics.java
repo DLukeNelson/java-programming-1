4eb23429009ac5aef35e0c0c2375ed4ed5474d4a
Luke <dlnelson@ualberta.ca>
2021-04-27 09:11:56 -0600

    SportStatistics find number of games for a team

--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -6,7 +6,40 @@ import java.util.Scanner;
 public class SportStatistics {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Filename:");
+        String fileName = scanner.nextLine();
+
+        ArrayList<SportStatistic> records = readRecordsFromFile(fileName);
+        
+        System.out.println("Team:");
+        String team = scanner.nextLine();
+        
+        int games = 0;
+        for (SportStatistic record: records) {
+            if (team.equals(record.getWinningTeam()) || team.equals(record.getLosingTeam())) {
+                games += 1;
+            }
+        }
+        
+        System.out.println("Games: " + games);
+
+    }
+
+    public static ArrayList<SportStatistic> readRecordsFromFile(String fileName) {
+        ArrayList<SportStatistic> statistics = new ArrayList<>();
+        try (Scanner fileScanner = new Scanner(Paths.get(fileName))) {
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                String[] parts = line.split(",");
+                SportStatistic stat = new SportStatistic(parts[0], parts[1], Integer.valueOf(parts[2]), Integer.valueOf(parts[3]));
+                statistics.add(stat);
+            }
+        } catch (Exception e) {
+            System.out.println("Unable to read file: " + fileName);
+        }
+        return statistics;
 
     }
 
