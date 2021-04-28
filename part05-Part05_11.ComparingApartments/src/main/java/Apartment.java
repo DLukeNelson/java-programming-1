5d3ae9e76b916f13ef8401bb32004d29fc86f06e
Luke <dlnelson@ualberta.ca>
2021-04-28 17:30:36 -0600

    ComparingApartments get price difference

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -3,16 +3,24 @@ public class Apartment {
 
     private int rooms;
     private int squares;
-    private int princePerSquare;
+    private int pricePerSquare;
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
-        this.princePerSquare = pricePerSquare;
+        this.pricePerSquare = pricePerSquare;
     }
     
     public boolean largerThan(Apartment other) {
         return this.squares > other.squares;
     }
 
+    public int price() {
+        return this.pricePerSquare * this.squares;
+    }
+    
+    public int priceDifference(Apartment other) {
+        int difference = this.price() - other.price();
+        return Math.abs(difference);
+    }
 }
