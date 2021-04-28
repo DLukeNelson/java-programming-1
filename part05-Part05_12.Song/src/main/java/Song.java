b21f6e109c0de904be750810785c61659f41e680
Luke <dlnelson@ualberta.ca>
2021-04-28 17:35:56 -0600

    Song exercise

--- part05-Part05_12.Song/src/main/java/Song.java
+++ part05-Part05_12.Song/src/main/java/Song.java
@@ -15,6 +15,23 @@ public class Song {
     public String toString() {
         return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
     }
+    
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        
+        if (!(other instanceof Song)) {
+            return false;
+        }
+        
+        Song that = (Song) other;
+        
+        return this.artist.equals(that.artist)
+                && this.name.equals(that.name)
+                && this.durationInSeconds == that.durationInSeconds;
+    }
 
 
 }
