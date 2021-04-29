420052dadf23e0c0e73c0c2fe3adf37d0273614d
Luke <dlnelson@ualberta.ca>
2021-04-29 09:09:34 -0600

    Books exercise add equals to Book

--- part05-Part05_14.Books/src/main/java/Book.java
+++ part05-Part05_14.Books/src/main/java/Book.java
@@ -17,4 +17,19 @@ public class Book {
         return publicationYear;
     }
 
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        
+        if (!(other instanceof Book)) {
+            return false;
+        }
+        
+        Book that = (Book) other;
+        
+        return this.name.equals(that.name)
+                && this.publicationYear == that.publicationYear;
+    }
 }
