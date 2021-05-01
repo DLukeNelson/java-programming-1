aaabdcb6652e26a5d18a35f4d57818c2b159ea13
Luke <dlnelson@ualberta.ca>
2021-05-01 15:41:28 -0600

    Exercises exercise first half of TDD

--- part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
+++ part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
@@ -1,4 +1,28 @@
 
-public class ExerciseManagement {
+import java.util.ArrayList;
 
+
+public class ExerciseManagement {
+    private ArrayList<String> exercises;
+    private ArrayList<String> completed;
+
+    public ExerciseManagement() {
+        this.exercises = new ArrayList<>();
+        this.completed = new ArrayList<>();
+    }
+    public ArrayList<String> exerciseList() {
+        return this.exercises;
+    }
+    
+    public void add(String exercise) {
+        this.exercises.add(exercise);
+    }
+    
+    public void markAsCompleted(String exercise) {
+        this.completed.add(exercise);
+    }
+    
+    public boolean isCompleted(String exercise) {
+        return this.completed.contains(exercise);
+    }
 }
