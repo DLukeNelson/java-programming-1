de1737939646889205d939a3718e20c9eab94583
Luke <dlnelson@ualberta.ca>
2021-05-01 10:01:28 -0600

    CargoHold exercise Hold class

---
+++ part06-Part06_08.CargoHold/src/main/java/Hold.java
@@ -0,0 +1,42 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author D Luke Nelson <david.luke.nelson@gmail.com>
+ */
+public class Hold {
+    private ArrayList<Suitcase> suitcases;
+    private int maxWeight;
+    
+    public Hold(int maxWeight) {
+        this.suitcases = new ArrayList<>();
+        this.maxWeight = maxWeight;
+    }
+
+    public void addSuitcase(Suitcase suitcase) {
+        int totalWeight = suitcase.totalWeight() + this.totalWeight();
+        if (totalWeight <= this.maxWeight) {
+            this.suitcases.add(suitcase);
+        }
+    }
+    
+    @Override
+    public String toString() {
+        return this.suitcases.size() + "suitcases (" + this.totalWeight() + " kg)";
+    }
+    
+    private int totalWeight() {
+        int weight = 0;
+        for (Suitcase suitcase: this.suitcases) {
+            weight += suitcase.totalWeight();
+        }
+        return weight;
+    }
+}
