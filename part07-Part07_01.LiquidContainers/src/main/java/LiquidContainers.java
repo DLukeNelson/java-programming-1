66f8087f1acccbad1c6c1779d4ce65479c3032fe
Luke <dlnelson@ualberta.ca>
2021-05-03 13:41:57 -0600

    LiquidContainers exercise add command

--- part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
+++ part07-Part07_01.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,14 +5,32 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int first = 0;
+        int second = 0;
 
 
         while (true) {
+            System.out.println("First: " + first + "/100");
+            System.out.println("Second: " + second + "/100");
             System.out.print("> ");
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
+            }
+            
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+            
+            if (command.equals("add")) {
+                if (amount < 0) {
+                    continue;
+                }
+                first += amount;
+                if (first > 100) {
+                    first = 100;
+                }
             }
 
         }
