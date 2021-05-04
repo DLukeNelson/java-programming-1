dfcc38e85a5b04e677e59c51bc5e1cf1abe15c56
Luke <dlnelson@ualberta.ca>
2021-05-04 08:53:55 -0600

    Sorting exercise smallest method

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -5,5 +5,16 @@ public class MainProgram {
         // write your test code here
         int[] array = {3, 1, 5, 99, 3, 12};
     }
+    
+    public static int smallest(int[] array) {
+        int smallest = array[0];
+        for (int i = 0; i < array.length; i ++) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+            }
+        }
+        return smallest;
+        
+    }
 
 }
