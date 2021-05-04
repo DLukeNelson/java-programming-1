823755865e4147f4f5aa9fd13f5d270ebba78f41
Luke <dlnelson@ualberta.ca>
2021-05-04 08:59:35 -0600

    Sorting exercise indexOfSmallest method

--- part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -8,13 +8,22 @@ public class MainProgram {
     
     public static int smallest(int[] array) {
         int smallest = array[0];
-        for (int i = 0; i < array.length; i ++) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] < smallest) {
                 smallest = array[i];
             }
         }
-        return smallest;
-        
+        return smallest; 
+    }
+    
+    public static int indexOfSmallest(int[] array) {
+        int smallestIndex = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < array[smallestIndex]) {
+                smallestIndex = i;
+            }
+        }
+        return smallestIndex;
     }
 
 }
