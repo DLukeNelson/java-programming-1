7ef8505b9a10cc1d487d99189e24546bd17539f4
Luke <dlnelson@ualberta.ca>
2021-05-05 11:34:51 -0600

    Use all the new classes

--- part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
+++ part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
@@ -7,6 +7,10 @@ public class RecipeSearch {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        RecipeList recipeList = new RecipeList();
+        
+        UserInterface ui = new UserInterface(scanner, recipeList);
+        ui.start();
 
     }
 
